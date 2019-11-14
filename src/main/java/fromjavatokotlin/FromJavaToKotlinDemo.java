070c33e591e81f38479075bb1fc2d7824c220c6d
Ivan Osipov <ivan.osipov@tra.ai>
2019-11-14 09:48:35 +0300

    added compatibility with binding

--- src/main/java/fromjavatokotlin/FromJavaToKotlinDemo.java
+++ src/main/java/fromjavatokotlin/FromJavaToKotlinDemo.java
@@ -3,11 +3,6 @@ package fromjavatokotlin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -17,14 +12,5 @@ public class FromJavaToKotlinDemo {
 
   public static void main(String[] args) {
     SpringApplication.run(FromJavaToKotlinDemo.class);
-  }
-
-  @Bean
-  public Docket swagger() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("fromjavatokotlin"))
-        .paths(PathSelectors.any())
-        .build();
   }
 }
--- src/main/java/fromjavatokotlin/model/Identifiable.java
+++ src/main/java/fromjavatokotlin/model/Identifiable.java
@@ -2,9 +2,6 @@ package fromjavatokotlin.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
-
-import static fromjavatokotlin.constant.DocumentConstants.ID_FIELD;
 
 @Data
 public class Identifiable {
